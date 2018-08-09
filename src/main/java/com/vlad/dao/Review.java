8e2dbc0303ff82f3664947f35a2062507db893d8
Vlad <vladmartishevskii@gmail.com>
2018-08-09 15:13:39 +0300

    Created kafka consumer

--- src/main/java/com/vlad/dao/Review.java
+++ src/main/java/com/vlad/dao/Review.java
@@ -1,8 +1,44 @@
 package com.vlad.dao;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Review {
-
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String positive;
+	private String negative;
+	private int usingPeriod;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPositive() {
+		return positive;
+	}
+	public void setPositive(String positive) {
+		this.positive = positive;
+	}
+	public String getNegative() {
+		return negative;
+	}
+	public void setNegative(String negative) {
+		this.negative = negative;
+	}
+	public int getUsingPeriod() {
+		return usingPeriod;
+	}
+	public void setUsingPeriod(int usingPeriod) {
+		this.usingPeriod = usingPeriod;
+	}
+	
 }
--- src/main/java/com/vlad/services/ReviewConsumer.java
+++ src/main/java/com/vlad/services/ReviewConsumer.java
@@ -1,8 +1,25 @@
 package com.vlad.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.vlad.dao.Review;
+import com.vlad.dao.repository.ReviewRepository;
 
 @Service
 public class ReviewConsumer {
+	
+	@Autowired
+	private ReviewRepository reviewRepository;
+	
+	private Gson gson = new Gson();
+	
+	@KafkaListener(topics = "${kafka.topic}")
+	public void listen(@Payload String message) {
+		reviewRepository.save(gson.fromJson(message, Review.class));
+	}
 
 }
